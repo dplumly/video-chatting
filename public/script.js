8b9fc64140f9743dba5d3a845cf19c8caae8909b
Donnie Plumly <donnie.plumly@gmail.com>
2020-08-01 18:28:07 -0400

    try same port

--- public/script.js
+++ public/script.js
@@ -1,9 +1,8 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  // key: 'peerjs',
-  // path: '/peerjs',
-  host: 'video-chatting-app.herokuapp.com',
+  key: 'peerjs',
+  host: 'video-chatting-app.herokuapp.com//',
   secure: true,
   port: 443
 })
